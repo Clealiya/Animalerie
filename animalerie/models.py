608e5ad7aa885aaa156e2a513807e198bbaa959d
Clealiya <clea.han@centrale-marseille.fr>
2022-11-19 20:42:39 +0100

    front fait

--- animalerie/models.py
+++ animalerie/models.py
@@ -5,17 +5,19 @@ from django.utils import timezone
 class Equipement(models.Model):
     id_equip = models.CharField(max_length=100, primary_key=True)
     disponibilite = models.CharField(max_length=20)
-    photo = models.CharField(max_length=200)
+    photo = models.ImageField()
+
     def __str__(self):
         return self.id_equip
- 
  
 class Animal(models.Model):
     id_animal = models.CharField(max_length=100, primary_key=True)
     etat = models.CharField(max_length=20)
     type = models.CharField(max_length=20)
     race = models.CharField(max_length=20)
-    photo = models.CharField(max_length=200)
+    photo = models.ImageField()
     lieu = models.ForeignKey(Equipement, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.id_animal
+    
--- animalerie/views.py
+++ animalerie/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Equipement, Animal
+from .forms import MoveForm
 
-def accueil(request):
-    animaux = Animal.objects.all().order_by('id_animal')
-    equipements = Equipement.objects.all().order_by('id_equip')
-    return render(request, 'animalerie/accueil.html', {'animaux': animaux, 'equipements': equipements})
+def animal_list(request):
+    animaux = Animal.objects.all()
+    equipements = Equipement.objects.all()
+    return render(request, 'animalerie/animal_list.html', {'animaux': animaux, 'equipements': equipements})
 
-    
+def animal_detail(request, id_animal):
+    animal = get_object_or_404(Animal, id_animal=id_animal)
+    lieu = animal.lieu
+    form = MoveForm()
+    if form.is_valid():
+        ancien_lieu = get_object_or_404(Equipement, id_equip=animal.lieu.id_equip)
+        ancien_lieu.disponibilite = "libre"
+        ancien_lieu.save()
+        form.save()
+        nouveau_lieu = get_object_or_404(Equipement, id_equip=animal.lieu.id_equip)
+        nouveau_lieu.disponibilite = "occupé"
+        nouveau_lieu.save()
+        return redirect('animal_detail', id_animal=id_animal)
+    else:
+        form = MoveForm()
+        return render(request,
+                  'animalerie/animal_detail.html',
+                  {'animal': animal, 'lieu': lieu, 'form': form})
